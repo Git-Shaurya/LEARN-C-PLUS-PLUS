99799ec825b06b28b1c6b1f03bdd5d03a683ffd1
Shaurya <shaurya.35k@gmail.com>
2025-05-23 23:00:45 +0530

    1st c++

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"Hello world";
+    cout<<"Hi everyone";
+
+cout<<endl;
+cout<<endl;
+
+//Here cout<<endl is used to give gap between line and is never written with " " symbol. also cout<<"/n" can be used to do same work
+
+     cout<<"Hello world";
+    cout<<endl;
+    cout<<"Hi everyone";
+
+    cout<<endl;
+    cout<<endl; 
+
+    cout<<"Hello world"<<endl<<"Hi Everyone";
+    //we can write like this too
+
+    
+}
