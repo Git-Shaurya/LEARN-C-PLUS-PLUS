dc96497629336cfbf59d89c014e4570a0294fd8b
Shaurya <shaurya.35k@gmail.com>
2025-07-02 15:49:21 +0530

    change

--- learncpp.cpp
+++ learncpp.cpp
@@ -26,5 +26,41 @@ cout<<endl;
     cout<<endl;
     cout<<"4+3";
 //here whene i printed 4+3 without the "" then it performed the calculation but when i did put it with the symbol it did not cause it considered it as a text.
+cout<<endl;
+cout<<endl;
 
+int x;
+x = 5;
+cout<<x;
+
+cout<<endl;
+
+int y;
+y = 5;
+cout<<y;
+
+cout<<endl;
+cout<<endl;
+
+cout<<x+y;
+
+cout<<endl;
+cout<<endl;
+
+
+//this can also be written as 
+
+int a;
+a = 5;
+int b;
+b = 5;
+cout<<a+b;
+
+cout<<endl;
+
+int c;
+c = 5;
+int d;
+d = 7;
+cout<<c+d;
 }
